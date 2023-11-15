9520ae09a7b567c2cdedd038ae0c41aca9ede159
Chirag Dhorajiya <100044854+ChiragD071@users.noreply.github.com>
2023-11-14 21:14:41 -0500

    14 November chirag

--- VistaCart/frontend/src/components/ContactUs.js
+++ VistaCart/frontend/src/components/ContactUs.js
@@ -1,82 +1,152 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import '../css/loginSigup.css';
 import Logo from '../images/vistacartlogomini.png';
 
 function ContactUs() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
+  const [nameError, setNameError] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [messageError, setMessageError] = useState('');
 
-    return (
-      <>
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-sm-7">
-              <div className="card mt-5">
-                <div className="card-body">
-                  <h5 className="card-title text-center">
-                    Send us your concern
-                        {/* <img className="logo" src={Logo} alt="Vista cart Logo" /> Vistacart */}
-                    </h5>
-                    <hr />
-                  <form>
-                    <div className="mb-3 form-group">
-                      <label htmlFor="name" className="form-label card-text">Name</label>
-                        <input type="text"
-                          className={`form-control`}
-                          id="name"
-                          name="name"
-                          placeholder="Enter your First name and last name" />
-                        <div className="invalid-feedback text-white">Please enter correct name</div>
+  const validateEmail = (email) => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Validate Name
+    if (!/^[a-zA-Z ]+$/.test(name)) {
+      setNameError('Please enter a valid name (only characters and spaces)');
+    } else {
+      setNameError('');
+    }
+
+    // Validate Email
+    if (!validateEmail(email)) {
+      setEmailError('Please enter a valid email address');
+    } else {
+      setEmailError('');
+    }
+
+    // Validate Message
+    if (message.trim() === '') {
+      setMessageError('Please enter a message to submit the form');
+    } else {
+      setMessageError('');
+    }
+
+    // Check if there are any errors
+    if (!nameError && !emailError && !messageError) {
+      console.log('Form submitted:', { name, email, subject, message });
+      // Add your form submission logic here
+    }
+  };
+
+  return (
+    <>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-sm-7">
+            <div className="card mt-5">
+              <div className="card-body">
+                <h5 className="card-title text-center">
+                  Send us your concern
+                </h5>
+                <hr />
+                <form onSubmit={handleSubmit}>
+                  <div className="mb-3 form-group">
+                    <label htmlFor="name" className="form-label card-text">
+                      Name
+                    </label>
+                    <input
+                      type="text"
+                      className={`form-control ${nameError ? 'is-invalid' : ''}`}
+                      id="name"
+                      name="name"
+                      placeholder="Enter your First name and last name"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                    />
+                    <div className={nameError ? 'invalid-feedback' : 'd-none'}>
+                      {nameError}
                     </div>
-                    <div className="mb-3">
-                      <label htmlFor="email" className="form-label">Email</label>
-                        <input type="text"
-                          className={`form-control`}
-                          placeholder="Enter your email" />
-                        <div className="invalid-feedback text-white">Please enter correct email</div>
-                                    </div>
-                                    <div className="mb-3">
-                      <label htmlFor="subject" className="form-label">Subject</label>
-                        <input type="text"
-                          className={`form-control`}
-                          placeholder="Enter subject" />
-                        <div className="invalid-feedback text-white">Please enter correct subject</div>
-                                    </div>
-                                    <div className="mb-3">
-                      <label htmlFor="message" className="form-label">Message</label>
-                        <textarea type="text"
-                          className={`form-control`}
-                          placeholder="Enter message" />
-                        <div className="invalid-feedback text-white">Please enter correct message</div>
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="email" className="form-label">
+                      Email
+                    </label>
+                    <input
+                      type="text"
+                      className={`form-control ${emailError ? 'is-invalid' : ''}`}
+                      placeholder="Enter your email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <div className={emailError ? 'invalid-feedback' : 'd-none'}>
+                      {emailError}
                     </div>
-                    <button type="submit" className="btn btn-custom">Submit</button>
-                  </form>
-                </div>
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="subject" className="form-label">
+                      Subject
+                    </label>
+                    <input
+                      type="text"
+                      className={`form-control`}
+                      placeholder="Enter subject"
+                      value={subject}
+                      onChange={(e) => setSubject(e.target.value)}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="message" className="form-label">
+                      Message
+                    </label>
+                    <textarea
+                      className={`form-control ${messageError ? 'is-invalid' : ''}`}
+                      placeholder="Enter message"
+                      value={message}
+                      onChange={(e) => setMessage(e.target.value)}
+                    />
+                    <div className={messageError ? 'invalid-feedback' : 'd-none'}>
+                      {messageError}
+                    </div>
+                  </div>
+                  <button type="submit" className="btn btn-custom">
+                    Submit
+                  </button>
+                </form>
               </div>
-              
             </div>
+          </div>
 
-            <div className="col-sm-3">
-              <div className="card mt-5">
-                <div className="card-body">
+          <div className="col-sm-3">
+            <div className="card mt-5">
+              <div className="card-body">
                 <h5 className='m-2 text-center'>Contact Us</h5>
                 <hr />
-                  <h5>Address:</h5>
-                  <p>123, fedrick st, Kitchener, ON - N2M 4G8</p>
+                <h5>Address:</h5>
+                <p>123, fedrick st, Kitchener, ON - N2M 4G8</p>
 
-                  <h5>Phone:</h5>
-                  <p>1800-333-4444</p>
-                  <p>1800-333-4444</p>
-                  <p>1800-333-4444</p>
+                <h5>Phone:</h5>
+                <p>1800-333-4444</p>
+                <p>1800-333-4444</p>
+                <p>1800-333-4444</p>
 
-                  <h5>Email:</h5>
-                  <p>Support: support@vistacart.com</p>
-                  <p>Sales: sales@vistacart.com</p>
-                 </div>
-                </div>
+                <h5>Email:</h5>
+                <p>Support: support@vistacart.com</p>
+                <p>Sales: sales@vistacart.com</p>
+              </div>
             </div>
           </div>
         </div>
-      </>
-  )
+      </div>
+    </>
+  );
 }
 
-export default ContactUs
+export default ContactUs;
--- VistaCart/frontend/src/components/Signup.js
+++ VistaCart/frontend/src/components/Signup.js
@@ -1,38 +1,84 @@
 import React, { useState } from 'react';
-import '../css/loginSigup.css';
-import Logo from '../images/vistacartlogomini.png'; 
-import axios from "axios";
+import '../css/loginSigup.css'; // Ensure this path matches your CSS file
+import Logo from '../images/vistacartlogomini.png'; // Make sure this path matches your image
+import axios from 'axios';
+
 function Signup() {
-  
-  const [formErrors, setFormErrors] = useState(null);
+  const [formErrors, setFormErrors] = useState({});
   const [isSuccess, setIsSuccess] = useState(false);
   const [isFailed, setIsFailed] = useState(false);
-  const [firstName, setFirstName] = useState("");
+  const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
-  const [phone, setPhone] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [retypePassword, setRetypePassword] = useState("");
-  const [successMsg, setSuccessMsg] = useState("");
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [retypePassword, setRetypePassword] = useState('');
+  const [successMsg, setSuccessMsg] = useState('');
 
+  const isPasswordValid =
+    password.length >= 8 &&
+    /[A-Z]/.test(password) &&
+    /[a-z]/.test(password) &&
+    /[!@#$%^&*]/.test(password);
 
-  
+  const isPasswordMatch = password === retypePassword;
+
+  const getStrength = () => {
+    if (!password) return 0;
+    if (password.length >= 8 && isPasswordValid) return 3;
+    return 1;
+  };
+
   const handleFormSubmit = (e) => {
-     e.preventDefault();
-    const formErrors = {
-      firstName: !firstName,
-      lastName: !lastName,
-      phone: !phone,
-      email: !email,
-      password: !password,
-      retypePassword: !retypePassword || password !== retypePassword,
-    };
-    
-    setIsSuccess(false);
-    setIsFailed(false);
-    setFormErrors({ ...formErrors });
+    e.preventDefault();
+    const newFormErrors = {};
 
-    if (Object.values(formErrors).some((v) => v)) return;
+    if (!firstName) {
+      newFormErrors.firstName = 'Please enter your First Name';
+    }
+
+    if (!lastName) {
+      newFormErrors.lastName = 'Please enter your Last Name';
+    }
+
+    if (!phone) {
+      newFormErrors.phone = 'Please enter your Mobile Number';
+    }
+
+    if (!email) {
+      newFormErrors.email = 'Please enter your Email';
+    }
+
+    if (!/^[a-zA-Z]*$/.test(firstName)) {
+      newFormErrors.firstName = 'First Name should contain only characters.';
+    }
+
+    if (!/^[a-zA-Z]*$/.test(lastName)) {
+      newFormErrors.lastName = 'Last Name should contain only characters.';
+    }
+
+    if (!/^\d{10}$/.test(phone)) {
+      newFormErrors.phone = 'Phone number should contain 10 digits only.';
+    }
+
+    if (!/\S+@\S+\.\S+/.test(email)) {
+      newFormErrors.email = 'Please enter a valid email address.';
+    }
+
+    if (!isPasswordValid || !password) {
+      newFormErrors.password =
+        'Password must be at least 8 characters and contain an uppercase letter, a lowercase letter, and a special character';
+    }
+
+    if (!isPasswordMatch || !retypePassword) {
+      newFormErrors.retypePassword = 'Password does not match';
+    }
+
+    setFormErrors(newFormErrors);
+
+    if (Object.keys(newFormErrors).length > 0) {
+      return;
+    }
 
     let userData = {
       firstName: firstName,
@@ -40,27 +86,24 @@ function Signup() {
       phone: phone,
       email: email,
       password: password,
-      userType: "customer"
-    }
+      userType: 'customer',
+    };
 
-
-    axios.post("http://localhost:8080/api/users/addUser", userData)
+    axios
+      .post('http://localhost:8080/api/users/addUser', userData)
       .then((response) => {
         window.scrollTo(0, 0);
-        // console.log(response);
         if (response.status === 200) {
           if (response.data.status === 1) {
-          // alert('Account created successfully.');
             setIsSuccess(true);
             setIsFailed(false);
-            
             setSuccessMsg(response.data.msg);
-            setFirstName("");
+            setFirstName('');
             setLastName('');
-            setPhone("");
-            setEmail("");
-            setPassword("");
-            setRetypePassword("");
+            setPhone('');
+            setEmail('');
+            setPassword('');
+            setRetypePassword('');
           } else {
             setIsSuccess(false);
             setIsFailed(true);
@@ -71,113 +114,169 @@ function Signup() {
           setIsFailed(true);
           setSuccessMsg('Something went wrong. Please try again later!');
         }
-    })
+      });
+  };
 
-   };
+  const getPasswordColor = () => {
+    const strength = getStrength();
+    if (strength === 1) {
+      return 'red';
+    } else if (strength === 3) {
+      return 'green';
+    }
+    return 'yellow';
+  };
 
   return (
-          <>
-            <div className="container">
-              <div className="row justify-content-center">
+    <>
+      <div className="container">
+        <div className="row justify-content-center">
           <div className="col-md-4">
             {isSuccess && (
               <div className="alert alert-success mt-3" role="alert">
                 <b>Success!</b> {successMsg}
               </div>
             )}
-            
+
             {isFailed && (
               <div className="alert alert-danger mt-3" role="alert">
                 <b>Failed!</b> {successMsg}
               </div>
-              )}
-                  <div className="card mt-5">
-                    <div className="card-body">
-                <h5 className="card-title text-center">
-                  VISTACART
-                            {/* <img className="logo" src={Logo} alt="Vista cart Logo" /> Vistacart */}
-                        </h5>
-                        <hr />
-                      <form onSubmit={handleFormSubmit}>
-                        <div className="mb-3">
-                          <label htmlFor="firstName" className="form-label">First Name</label>
-                          <input type="text" 
-                            className={`form-control ${formErrors && (formErrors?.firstName ? "is-invalid" : "is-valid")}`}
-                            id="firstName"
-                            name="firstName"
-                            value={firstName}
-                            onChange={(e) => {setFirstName(e.currentTarget.value);}}
-                            placeholder="Enter your first name" />
-                        
-                          <div className="invalid-feedback text-white">Please enter correct First Name</div>
-                        </div>
-                        <div className="mb-3">
-                          <label htmlFor="lastName" className="form-label">Last Name</label>
-                          <input type="text"
-                            className={`form-control ${formErrors && (formErrors?.lastName ? "is-invalid" : "is-valid")}`}
-                            id="lastName"
-                            name="lastName"
-                            value={lastName}
-                            onChange={(e) => {setLastName(e.currentTarget.value);}}
-                            placeholder="Enter your last name" />
-                          <div className="invalid-feedback text-white">Please enter correct Last Name</div>
-                        </div>
-                        <div className="mb-3">
-                          <label htmlFor="phone" className="form-label">Phone Number</label>
-                          <input type="text"
-                            className={`form-control ${formErrors && (formErrors?.phone ? "is-invalid" : "is-valid")}`}
-                            id="phone"
-                            name="phone"
-                            value={phone}
-                            onChange={(e) => {setPhone(e.currentTarget.value);}}
-                            placeholder="Enter your phone number" />
-                          <div className="invalid-feedback text-white">Please enter correct Mobile Number</div>
-                        </div>
-                        <div className="mb-3">
-                          <label htmlFor="email" className="form-label">Email</label>
-                          <input type="text"
-                            className={`form-control ${formErrors && (formErrors?.email ? "is-invalid" : "is-valid")}`}
-                            id="email"
-                            name="email"
-                            value={email}
-                            onChange={(e) => { setEmail(e.currentTarget.value); }}
-                            placeholder="Enter your email" />
-                          <div className="invalid-feedback text-white">Please enter correct Email</div>
-                        </div>
-                        <div className="mb-3">
-                          <label htmlFor="password" className="form-label">Password</label>
-                          <input type="password"
-                            className={`form-control ${formErrors && (formErrors?.password ? "is-invalid" : "is-valid")}`}
-                            id="password"
-                            name="password"
-                            value={password}
-                            onChange={(e) => {setPassword(e.currentTarget.value);}}
-                            placeholder="Enter your password" />
-                          <div className="invalid-feedback text-white">Please enter Password</div>
-                        </div>
-                        <div className="mb-3">
-                          <label htmlFor="retypePassword" className="form-label">Retype Password</label>
-                          <input type="password"
-                            className={`form-control ${formErrors && (formErrors?.retypePassword ? "is-invalid" : "is-valid")}`}
-                            id="retypePassword"
-                            name="retypePassword"
-                            value={retypePassword}
-                            onChange={(e) => { setRetypePassword(e.currentTarget.value); }}
-                            placeholder="Retype your password" />
-                          <div className="invalid-feedback text-white">Password does not match</div>
-                        </div>
-                        <button type="submit" className="btn btn-custom">Sign Up</button>
-                      </form>
-                      <div className="mt-3">
-                        <p>Already have an account? <a href="/login">Login here</a></p>
-                      </div>
-                    </div>
+            )}
+            <div className="card mt-5">
+              <div className="card-body">
+                <h5 className="card-title text-center">VISTACART</h5>
+                <hr />
+                <form onSubmit={handleFormSubmit}>
+                  <div className="mb-3">
+                    <label htmlFor="firstName" className="form-label">
+                      First Name
+                    </label>
+                    <input
+                      type="text"
+                      className={`form-control ${formErrors.firstName ? 'is-invalid' : ''}`}
+                      id="firstName"
+                      name="firstName"
+                      value={firstName}
+                      onChange={(e) => setFirstName(e.currentTarget.value)}
+                      placeholder="Enter your first name"
+                    />
+                    {formErrors.firstName && (
+                      <div className="invalid-feedback text-danger">{formErrors.firstName}</div>
+                    )}
                   </div>
+                  <div className="mb-3">
+                    <label htmlFor="lastName" className="form-label">
+                      Last Name
+                    </label>
+                    <input
+                      type="text"
+                      className={`form-control ${formErrors.lastName ? 'is-invalid' : ''}`}
+                      id="lastName"
+                      name="lastName"
+                      value={lastName}
+                      onChange={(e) => setLastName(e.currentTarget.value)}
+                      placeholder="Enter your last name"
+                    />
+                    {formErrors.lastName && (
+                      <div className="invalid-feedback text-danger">{formErrors.lastName}</div>
+                    )}
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="phone" className="form-label">
+                      Phone Number
+                    </label>
+                    <input
+                      type="text"
+                      className={`form-control ${formErrors.phone ? 'is-invalid' : ''}`}
+                      id="phone"
+                      name="phone"
+                      value={phone}
+                      onChange={(e) => setPhone(e.currentTarget.value)}
+                      placeholder="Enter your phone number"
+                    />
+                    {formErrors.phone && (
+                      <div className="invalid-feedback text-danger">{formErrors.phone}</div>
+                    )}
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="email" className="form-label">
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      className={`form-control ${formErrors.email ? 'is-invalid' : ''}`}
+                      id="email"
+                      name="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.currentTarget.value)}
+                      placeholder="Enter your email"
+                    />
+                    {formErrors.email && (
+                      <div className="invalid-feedback text-danger">{formErrors.email}</div>
+                    )}
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="password" className="form-label">
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      className={`form-control ${formErrors.password ? 'is-invalid' : ''}`}
+                      id="password"
+                      name="password"
+                      value={password}
+                      onChange={(e) => setPassword(e.currentTarget.value)}
+                      placeholder="Enter your password"
+                    />
+                    {formErrors.password && (
+                      <div className="invalid-feedback text-danger">{formErrors.password}</div>
+                    )}
+                  </div>
+                  <div className="password-strength">
+                    <label style={{ color: 'grey' }}>Password Strength</label>
+                    <br />
+                    <progress
+                      max="3"
+                      value={getStrength()}
+                      style={{ color: getPasswordColor() }}
+                    />
+                  </div>
+
+                  <div className="mb-3">
+                    <label htmlFor="retypePassword" className="form-label">
+                      Retype Password
+                    </label>
+                    <input
+                      type="password"
+                      className={`form-control ${formErrors.retypePassword ? 'is-invalid' : ''}`}
+                      id="retypePassword"
+                      name="retypePassword"
+                      value={retypePassword}
+                      onChange={(e) => setRetypePassword(e.currentTarget.value)}
+                      placeholder="Retype your password"
+                    />
+                    {formErrors.retypePassword && (
+                      <div className="invalid-feedback text-danger">{formErrors.retypePassword}</div>
+                    )}
+                  </div>
+
+                  <button type="submit" className="btn btn-custom">
+                    Sign Up
+                  </button>
+                </form>
+                <div className="mt-3">
+                  <p>
+                    Already have an account? <a href="/login">Login here</a>
+                  </p>
                 </div>
               </div>
             </div>
-          </>
-  )
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Signup
+export default Signup;
